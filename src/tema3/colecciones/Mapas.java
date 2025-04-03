ac2f5dd7a7604fc6579c1164bdc16746715c7af2
txipi <txipi@lt-garaizar>
2025-04-03 15:01:57 +0200

    Mapas

---
+++ src/tema3/colecciones/Mapas.java
@@ -0,0 +1,24 @@
+package tema3.colecciones;
+
+public class Mapas {
+
+	public static void main(String[] args) {
+		/*
+		 * Python:
+		 * 
+		 * libro = {
+		 *   "titulo": "Quijote",
+		 *   "autoria": "Cervantes",
+		 *   "anyo": 1506
+		 * }
+		 * 
+		 * HashMap: mapa ordenado con una tabla hash, las claves tengan hashCode()
+		 * 		Tiene sus claves en un HashSet
+		 * TreeMap: mapa ordenado con un árbol, las claves tienen que ser Comparables
+		 * 		Tiene sus claves en un TreeSet
+		 */
+		
+		
+	}
+
+}
